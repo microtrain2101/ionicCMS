2e608b1e78bd7219f11fec078e88f4b83fb4dacb
chad svastisalee <swasgrafx@gmail.com>
2021-03-25 10:30:13 -0500

    Generate Article Page and View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,10 @@ const routes: Routes = [
   {
     path: 'articles',
     loadChildren: () => import('./articles/articles.module').then( m => m.ArticlesPageModule)
+  },
+  {
+    path: 'article/:articleId',
+    loadChildren: () => import('./article/article.module').then( m => m.ArticlePageModule)
   }
 ];
 
