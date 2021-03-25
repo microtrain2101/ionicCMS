2b296afba97bd6d2acb77679795d531a70fbc7ae
chad svastisalee <swasgrafx@gmail.com>
2021-03-25 10:34:59 -0500

    Add Articles to the side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -7,19 +9,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  public appPages = [
+// export class AppComponent {
+  export class AppComponent implements OnInit {
+    public selectedIndex = 0;
+    public appPages = [
     { title: 'Login', url: '/login', icon: 'log-in' },
     { title: 'Logout', url: '/logout', icon: 'log-out' },
     { title: 'Register', url: '/register', icon: 'person-add' },
     { title: 'Users', url: '/users', icon: 'people' },
+    { title: 'Articles', url: '/articles', icon: 'newspaper'
+    },
   ];
 
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor(
-    private StatusBar: StatusBar,
-    private SplashScreen: SplashScreen
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
 
-  ) {}
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
 }
